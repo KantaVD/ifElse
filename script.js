b5b1abec399b781fbd2af84c177bd5a3f17e9a21
KantaVD <89583709+KantaVD@users.noreply.github.com>
2021-09-30 11:52:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const age= 40;
+
+if (age>18) {
+    console.log("Welkom in de kroeg")
+ } else {
+    console.log("Helaas ben je te jong om binnen te komen")
+
+ }
+
+ const isFemale= true;
+
+ if (isFemale) {
+    console.log("Je bent een vrouw")
+ } else {
+     console.log("Je hebt geen toegang tot de ladiesnight")
+ }
+ 
+ const driverStatus="bob";
+
+ if (driverStatus) {
+     console.log("Je krijgt gratis fris om je vriendinnen veilig thuis te brengen")
+ } else {
+     console.log("Zorg dat je met de BOB mee naar huis gaat")
+ }
